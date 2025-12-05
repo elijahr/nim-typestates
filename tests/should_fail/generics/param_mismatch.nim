a4d15fd92ac9017f3505bf89a11b4706719631c3
elijahr <elijahr@users.noreply.github.com>
2025-12-04 21:08:55 -0600

    test: add generic typestate should-fail tests

---
+++ tests/should_fail/generics/param_mismatch.nim
@@ -0,0 +1,21 @@
+## Test: Generic typestate with undeclared transition should fail
+## Expected error: Undeclared transition
+import ../../../src/typestates
+
+type
+  Box[T] = object
+    value: T
+  Empty[T] = distinct Box[T]
+  Full[T] = distinct Box[T]
+  Overflow[T] = distinct Box[T]  # Not in transitions
+
+typestate Box[T]:
+  isSealed = false
+  strictTransitions = false
+  states Empty[T], Full[T]
+  transitions:
+    Empty[T] -> Full[T]
+
+# Wrong: Overflow is not a declared transition destination
+proc overflow[T](b: Full[T]): Overflow[T] {.transition.} =
+  Overflow[T](Box[T](b))
